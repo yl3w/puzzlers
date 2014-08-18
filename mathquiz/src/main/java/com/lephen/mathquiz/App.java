a57de956cd66935ac361f58bdcd28a0c4157a6b5
Bhaskar Maddala <bhaskar@tumblr.com>
2014-08-17 22:30:55 -0400

    Initial project configuration and commit

---
+++ mathquiz/src/main/java/com/lephen/mathquiz/App.java
@@ -0,0 +1,13 @@
+package com.lephen.mathquiz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
